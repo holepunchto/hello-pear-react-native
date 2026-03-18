3d0d0d82297f232178c129d93065dd09de32c794
Geordan Gesink <georgegeordan@gmail.com>
2026-03-18 17:32:23 +0100

    move swarming outside module

--- pearend/worker.js
+++ pearend/worker.js
@@ -2,6 +2,10 @@
 const RPC = require('bare-rpc')
 const { IPC } = Bare
 const PearRuntime = require('pear-mobile')
+const Corestore = require('corestore')
+const Hyperswarm = require('hyperswarm')
+const path = require('bare-path')
+const dir = require('bare-storage')
 const goodbye = require('graceful-goodbye')
 const { version, upgrade, name, productName } = require('../package.json')
 
@@ -15,21 +19,34 @@ const rpc = new RPC(IPC, (req) => {
 const req = rpc.request(1)
 req.send('Hello from Worklet!👋🍐')
 
-const pear = new PearRuntime({ version, upgrade, name: appName, updates })
-pear.updater.on('error', (err) => console.error(err))
-pear.updater.on('updated', async () => {
-  await pear.updater.applyUpdate()
-  const req = rpc.request(0)
-  req.send('update')
-})
-
 goodbye(async () => {
+  await swarm.destroy()
   await pear.close()
+  await store.close()
 })
 
 main()
 async function main() {
+  const store = new Corestore(path.join(dir.persistent(), 'pear-runtime/corestore'))
+  const keyPair = await store.createKeyPair('my-store')
+  const swarm = new Hyperswarm({ keyPair })
+  
+  const pear = new PearRuntime({ version, upgrade, name: appName, updates, swarm, store })
+  
+  pear.updater.on('error', (err) => console.error(err))
+  pear.updater.on('updated', async () => {
+    await pear.updater.applyUpdate()
+    const req = rpc.request(0)
+    req.send('update')
+  })
+
   await pear.ready()
+  swarm.on('connection', (connection) => store.replicate(connection))
+  swarm.join(pear.updater.drive.core.discoveryKey, {
+    client: true,
+    server: false
+  })
+
   const reqTwo = rpc.request(0)
   reqTwo.send(pear.updater.version.toString())
 }
